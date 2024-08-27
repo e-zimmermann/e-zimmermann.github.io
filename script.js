e13e12d270bc6e0526dbb80bdf5f36df1e5b116d
e-zimmermann <eric_zimmermann@posteo.de>
2024-08-27 08:37:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 document.getElementById('circle1').addEventListener('click', function (e) {
     window.location = "index.html";
 });
+document.getElementById('circle1_').addEventListener('click', function (e) {
+    window.location = "index.html";
+});
 document.getElementById('circle2').addEventListener('click', function (e) {
     window.location = "publications.html";
 });
@@ -15,7 +18,7 @@ document.getElementById('circle1').addEventListener('mouseover', function (e) {
 });
 document.getElementById('circle1').addEventListener('mouseleave', function (e) {
     document.getElementById('circle1_').setAttribute("fill", "rgb(231, 172, 33)");
-    
+
 });
 document.getElementById('circle1_').addEventListener('mouseover', function (e) {
     document.getElementById('circle1').setAttribute("fill", "rgb(247, 213, 134)");
